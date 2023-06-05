0ba949fea1e29835350958fa9d2c0cf6e0e5f96d
zackfei0721 <zackfei0721@gmail.com>
2023-06-05 17:46:17 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const ReduxContext = React.createContext();
+
+//Implement the custom connect function
+const myConnect = (mapStateToProps) => (Component) => {
+  return (props) => {
+    const {state, dispatch} = React.useContext(ReduxContext);
+    const stateToProps = mapStateToProps(state, props);
+    return <Component {...props} {...stateToProps} dispatch={dispatch} />
+  }
+};
+
+// Define the reducer
+const reducer = (state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1 };
+    default:
+      return state;
+  }
+};
+
+// Define the Counter component
+const Counter = ({ count, dispatch }) => (
+  <div>
+    <h4>Count: {count}</h4>
+    <button onClick={() => dispatch({ type: 'INCREMENT' })}>+ 1</button>
+  </div>
+);
+
+const ConnectedCounter = myConnect((state) => ({ count: state.count }))(Counter);
+
+const App = () => {
+  const [state, dispatch] = React.useReducer(reducer, { count: 0 });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReduxContext.Provider value={{state, dispatch}}>
+      <ConnectedCounter />
+    </ReduxContext.Provider>
   );
-}
+};
 
 export default App;
+
